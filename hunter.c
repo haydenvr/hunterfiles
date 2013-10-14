65568dc4a0a02d25bb3994161b6719113381b51a
Hayden William Arie Van Reyswoud <h.v.reyswoud@gmail.com>
2013-10-14 14:41:27 +1100

    added bases

--- hunter.c
+++ hunter.c
@@ -8,9 +8,29 @@
 
 
 void decideMove (HunterView gameState) {
+	char *locations[] = {
+	"AL", "AM", "AT", "BA", "BI", "BE", "BR", "BO", "BU", "BC", 
+	"BD", "CA", "CG", "CD", "CF", "CO", "CN", "DU", "ED", "FL",
+	"FR", "GA", "GW", "GE", "GO", "GR", "HA", "JM", "KL", "LE",
+	"LI", "LS", "LV", "LO", "MA", "MN", "MR", "MI", "MU", "NA",
+	"NP", "NU", "PA", "PL", "PR", "RO", "SA", "SN", "SR", "SJ",
+	"SO", "ST", "SW", "SZ", "TO", "VA", "VR", "VE", "VI", "ZA",
+	"ZU", "NS", "EC", "IS", "AO", "BB", "MS", "TS", "IO", "AS", 
+	"BS", "C?", "S?", "HI", "D1", "D2", "D3", "D4", "D5", "TP"
+	};
 	int round = getRound(gameState);
 	PlayerID id = getCurrentPlayer(gameState);
-    registerBestPlay("ZU", "I Wanna Stake You Dracula <3" );
+    LocationID move;
+	if (id == PLAYER_LORD_GODALMING ) {
+
+	} else if (id == PLAYER_DR_SEWARD) {
+
+	} else if (id == PLAYER_VAN_HELSING) {
+
+	} else if (id == PLAYER_MINA_HARKER) {
+
+	}
  
+	registerBestPlay(locations[move], "I Wanna Stake You Dracula <3" );
 }
 
